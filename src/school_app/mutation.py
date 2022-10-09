9e0c9cd210a35552626299e5df2fb3e219cc72d8
henryhe <hehenry14@gmail.com>
2022-10-09 22:43:24 +0200

    chore: add session handler

--- src/school_app/mutation.py
+++ src/school_app/mutation.py
@@ -1,16 +1,12 @@
 from src.school_app.models import db, Student
 from sqlalchemy.exc import SQLAlchemyError
+from src.common.rollback_handler import session_handler
 
+@session_handler
 def update_name(id: int, new_name: str):
     row = db.session.query(Student).one_or_none()
-    if row:
-        try:
-            row.name = new_name
-            db.session.add(row)
-            db.session.commit()
-            db.session.flush()
-        except SQLAlchemyError:
-            db.session.rollback()
-        finally:
-            db.session.close()
+    row.name = new_name
+    db.session.add(row)
+    db.session.commit()
+    db.session.flush()
 
--- src/school_app/query.py
+++ src/school_app/query.py
@@ -2,8 +2,10 @@ from tkinter import S
 from .models import Student, db
 from src.app import app
 from sqlalchemy.exc import SQLAlchemyError
+from src.common.rollback_handler import session_handler
 
 
+# @session_handler
 def get_students() -> list:
     """This function gets all students from the data base
 
