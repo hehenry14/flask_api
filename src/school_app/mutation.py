a299143cf9c86d8f9586c3a4b7b7cfa427e59cb7
henryhe <hehenry14@gmail.com>
2022-10-10 16:22:06 +0200

    fixed mistakes (#3)
    Co-authored-by: ZANDERS\hhe <h.he@zanders.eu>

--- src/school_app/mutation.py
+++ src/school_app/mutation.py
@@ -2,11 +2,11 @@ from src.school_app.models import db, Student
 from sqlalchemy.exc import SQLAlchemyError
 from src.common.rollback_handler import session_handler
 
+
 @session_handler
-def update_name(id: int, new_name: str):
-    row = db.session.query(Student).one_or_none()
+def update_name(student_id: int, new_name: str):
+    row = db.session.query(Student).filter(Student.id == student_id).one_or_none()
     row.name = new_name
     db.session.add(row)
     db.session.commit()
     db.session.flush()
-
