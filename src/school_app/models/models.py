0399a0b1c65684684b53d174c400ae9b58667ed6
henryhe <hehenry14@gmail.com>
2022-10-10 22:53:22 +0200

    feat: added grades, one to many relationships (#4)

--- src/school_app/models/models.py
+++ src/school_app/models/models.py
@@ -1,11 +1,21 @@
+from bdb import GENERATOR_AND_COROUTINE_FLAGS
 from src.app import db
 
 
 class Student(db.Model):
+   __tablename__ = 'student'
    id = db.Column(db.Integer, primary_key = True)
    name = db.Column(db.String(100))
    city = db.Column(db.String(50))  
    addr = db.Column(db.String(200))
+   grades = db.relationship('Grade', lazy='subquery')
+
+class Grade(db.Model):
+   __tablename__ = 'grade'
+   id = db.Column(db.Integer, primary_key = True)
+   name = db.Column(db.String(100))
+
+   student_id = db.Column(db.Integer, db.ForeignKey("student.id"))
 
 
 # db.create_all()
--- tests/test_school_app/test_school_app.py
+++ tests/test_school_app/test_school_app.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
-from .factories.school_app_factories import StudentsFactory
-from src.school_app.models.models import db, Student
+from .factories.school_app_factories import StudentsFactory, GradeFactory
+from src.school_app.models.models import db, Student, Grade
 from src.app import create_app, db
 
 from src.school_app.query import get_students
@@ -19,11 +19,25 @@ class TestSchoolApp(TestCase):
         db.session.remove()
         db.drop_all()
 
-    def test_model(self):
+    def test_student_model_relationship(self):
+        student = StudentsFactory()
+        grades = db.session.query(Grade).all()
+        self.assertEqual(len(grades), 5)
+        for grade in grades:
+            self.assertEqual(grade.student_id, student.id)
+
+    def test_student_model(self):
         for i in range(5):
             StudentsFactory()
         result = get_students()
-        self.assertAlmostEquals(len(result), 5)
+        self.assertEquals(len(result), 5)
+    
+    def test_grade_model(self):
+        for i in range(5):
+            GradeFactory()
+        grades = db.session.query(Grade).all()
+        self.assertEquals(len(grades), 5)
+
 
     def test_update_name(self):
         student = StudentsFactory()
