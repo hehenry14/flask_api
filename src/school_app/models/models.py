7035c148bfcc03de0bb1374c1758f65928fb57e4
henryhe <hehenry14@gmail.com>
2022-10-10 12:14:23 +0200

    adding teacher

--- src/school_app/models/models.py
+++ src/school_app/models/models.py
@@ -6,6 +6,16 @@ class Student(db.Model):
    name = db.Column(db.String(100))
    city = db.Column(db.String(50))  
    addr = db.Column(db.String(200))
+   teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'),
+        nullable=False)
+
+
+class Teacher(db.Model):
+   id = db.Column(db.Integer, primary_key = True)
+   name = db.Column(db.String(100))
+   city = db.Column(db.String(50))  
+   addr = db.Column(db.String(200))
+   students = db.relationship('Student', backref='teacher', lazy=True)
 
 
 # db.create_all()
