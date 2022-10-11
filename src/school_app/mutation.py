8caea8801e14296ce3fe9726ab9c62dd1ddfd78d
henryhe <hehenry14@gmail.com>
2022-10-11 13:15:48 +0200

    adding the function to add grades, and adding unit test (#5)
    * adding the function to add grades, and adding unit test
    
    * fix: the session closed too early to do the test.
    
    Co-authored-by: ZANDERS\hhe <h.he@zanders.eu>

--- src/school_app/mutation.py
+++ src/school_app/mutation.py
@@ -1,4 +1,4 @@
-from src.school_app.models import db, Student
+from src.school_app.models import db, Student, Grade
 from sqlalchemy.exc import SQLAlchemyError
 from src.common.rollback_handler import session_handler
 
@@ -7,6 +7,15 @@ from src.common.rollback_handler import session_handler
 def update_name(student_id: int, new_name: str):
     row = db.session.query(Student).filter(Student.id == student_id).one_or_none()
     row.name = new_name
+    db.session.add(row)
+    db.session.commit()
+    db.session.flush()
+
+
+@session_handler
+def update_grade(student_id: int, grade_id: int):
+    row = db.session.query(Grade).filter(Grade.id == grade_id).one_or_none()
+    row.student_id = student_id
     db.session.add(row)
     db.session.commit()
     db.session.flush()
